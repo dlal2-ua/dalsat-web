27bf71fbca77da8da9cf13940edcc4a53f115e53
Alejandro Sanchez Tilve <asts3@gcloud.ua.es>
2026-05-16 14:05:54 +0200

    formulario

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -13,7 +13,7 @@ export default function ContactForm() {
     const data = new FormData(form);
 
     try {
-      const res = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+      const res = await fetch('https://formspree.io/f/xlgzqkva', {
         method: 'POST',
         body: data,
         headers: { Accept: 'application/json' },
